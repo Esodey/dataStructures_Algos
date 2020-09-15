8683d882082fa7ba255e807de2c8b216d1a2c545
Erik Soderholm <esodey@gmail.com>
2020-09-15 09:15:23 -0700

    "Add unshift method to SLL"

--- SLL.js
+++ SLL.js
@@ -54,6 +54,16 @@ class SinglyLinkedList {
     return currentHead;
   }
 
-
+  unshift (val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 
 }
